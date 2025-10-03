1e35cc1e8d72cfbdae97be173d4ddb1159bdbb71
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-10-03 12:08:24 +0530

    Update ws.js

--- frontend/src/ws.js
+++ frontend/src/ws.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 export function connectWS() {
-    return io('https://chat-online-47b7.vercel.app/');
+    return io('https://chat-online-47b7.vercel.app');
 }
